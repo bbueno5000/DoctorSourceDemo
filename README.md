11e06632bbc27700ba48b5543fbfafa5f36158b5
B. Bueno <bbueno25@hotmail.com>
2020-09-02 09:45:46 -0600

    update

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
-## Overview
+# doctor_source_demo
 
-This is the code for [this](https://youtu.be/J9kbZ5I8gdM) video on Youtube by Siraj Raval on how to build a biomedical startup. I basically saw [these](https://github.com/re-search/DocProduct) Colab demos of this BioBERT model answering medical questions pretty acurately and got hype. I thought, why not apply [this](https://github.com/rxlabz/sytody) type of interface to that as a backend, paywall it, and hey! You've got yourself a business, serving the underserved st scale at a very low cost. This app works, but there are still TODOs, i'll list them below. 
+A new Flutter application.
 
-## Usage
+## Getting Started
 
-Install [flutter](http://flutter.io). Then, install all dependencies and run using these commands. I prefer running it using Android Studio, but terminal works too. 
+This project is a starting point for a Flutter application.
 
-```bash
-flutter packages get
-flutter run
-```
+A few resources to get you started if this is your first Flutter project:
 
-:tv: [Video demo](https://youtu.be/7MGuNZfgGWw)
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
 
-## TODOs - Make a PR if you fix any
-
-- Take the model [here](https://colab.research.google.com/drive/11hAr1qo7VCSmIjWREFwyTFblU2LVeh1R) and integrate it into the app. My previous healthcare [app](https://github.com/llSourcell/How_to_Build_a_healthcare_startup) nicely integrated tensorflow lite. 
-- Parse the input using keyword recognition. You can use an existing API like [this](https://www.twinword.com/api/text-classification.php) to categorize words as symptoms, medications, or side effects. Train a new model on a health dataset for the most accuracy, have it learn to classify those 3 keywords from a given corpus. Once the input is parsed properly into those categories, it can be fed into the existing model. 
-- Finish Integrating Firebase
-- Finish Integrating Payments
-
-## Credits
-
-[rxlabs](https://github.com/rxlabz/sytody) and the Doc Product team. Also the Flutter team. 
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/task.dart
+++ lib/task.dart
@@ -1,37 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Task {
+class Task
+{
   int taskId;
   String label;
   bool complete;
-
   Task({this.taskId, this.label, this.complete = false});
 }
 
-class TaskWidget extends StatelessWidget {
+class TaskWidget extends StatelessWidget
+{
   final String label;
   final VoidCallback onDelete;
   final VoidCallback onComplete;
 
   TaskWidget({this.label, this.onDelete, this.onComplete});
 
-  Widget _buildDissmissibleBackground(
-    {Color color,
-      IconData icon,
-      FractionalOffset align = FractionalOffset.centerLeft}) =>
-    new Container(
-      height: 42.0,
-      color: color,
-      child: new Icon(icon, color: Colors.white70),
-      alignment: align,
-    );
+  Widget _buildDissmissibleBackground({
+    Color color,
+    IconData icon,
+    FractionalOffset align = FractionalOffset.centerLeft
+  }) => new Container(
+    height: 42.0,
+    color: color,
+    child: new Icon(icon, color: Colors.white70),
+    alignment: align,
+  );
 
   bool test = false;
 
   @override
   Widget build(BuildContext context) {
-
     if(test = false) {
       test = true;
       return new Container(
@@ -40,20 +40,23 @@ class TaskWidget extends StatelessWidget {
               direction: DismissDirection.horizontal,
               child: new Align(
                   alignment: FractionalOffset.centerLeft,
-                  child:
-                  new Padding(
-                      padding: new EdgeInsets.all(10.0), child: new Text(label, style: TextStyle(color: Colors.blue)))),
+                  child: new Padding(
+                      padding: new EdgeInsets.all(10.0),
+                      child: new Text(label, style: TextStyle(color: Colors.blue)),
+                  ),
+              ),
               key: new Key(label),
               background: _buildDissmissibleBackground(
-                  color: Colors.lime, icon: Icons.check),
+                  color: Colors.lime,
+                  icon: Icons.check),
               secondaryBackground: _buildDissmissibleBackground(
                   color: Colors.red,
                   icon: Icons.delete,
                   align: FractionalOffset.centerRight),
               onDismissed: (direction) => direction == DismissDirection.startToEnd
-                  ? onComplete()
-                  : onDelete()));
-
+                  ? onComplete() : onDelete(),
+          ),
+      );
     }
     else {
       test = false;
@@ -63,22 +66,23 @@ class TaskWidget extends StatelessWidget {
               direction: DismissDirection.horizontal,
               child: new Align(
                   alignment: FractionalOffset.centerLeft,
-                  child:
-                  new Padding(
-                      padding: new EdgeInsets.all(10.0), child: new Text(label, style: TextStyle(color: Colors.black)))),
+                  child: new Padding(
+                      padding: new EdgeInsets.all(10.0),
+                      child: new Text(label, style: TextStyle(color: Colors.black))
+                  ),
+              ),
               key: new Key(label),
               background: _buildDissmissibleBackground(
-                  color: Colors.lime, icon: Icons.check),
+                  color: Colors.lime,
+                  icon: Icons.check),
               secondaryBackground: _buildDissmissibleBackground(
                   color: Colors.red,
                   icon: Icons.delete,
                   align: FractionalOffset.centerRight),
               onDismissed: (direction) => direction == DismissDirection.startToEnd
-                  ? onComplete()
-                  : onDelete()));
-
-
+                  ? onComplete() : onDelete()
+          ),
+      );
     }
-
   }
 }
